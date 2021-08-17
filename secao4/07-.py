eca841db98ce20a72855daea98b97d81f9a43ddb
james <felipejames013@gmail.com>
2021-08-17 19:18:08 -0300

    novo

---
+++ secao4/07-.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        f = float(input('Digite a temperatura em Fahrenheit: '))
+        c = 5.0 * (f - 32.0) / 9.0
+    except ValueError:
+        print('Apenas valores númericos serão aceitos!')
+    else:
+        print(f'{f}ºf convertidos em celsius é igual a: {c:.2f}')
+        break
