8df634cd19c125e227097b3b7d9521d64e522ed4
JamesFelipe <77255316+SemajOliveira@users.noreply.github.com>
2021-08-17 21:29:26 -0300

    new

---
+++ secao4/08-.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        k = float(input('Digite a temperatura Kelvin por favor: '))
+        c = k - 273.15
+    except ValueError:
+        print('Apenas valores númericos serão aceitos!')
+    else:
+        print(f'Temperatura em Kelvin: {k}')
+        print(f'Mesma temperatura em Celsius: {c}')
+        break
