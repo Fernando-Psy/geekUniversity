2a35e2ec858741b2e41400edde6d8446a6f556b6
JamesFelipe <77255316+SemajOliveira@users.noreply.github.com>
2021-08-17 21:26:33 -0300

    Delete secao4 directory

--- secao4/01-.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        n = int(input('Digite um valor: '))  
-    except ValueError:
-        print('Apenas números inteiros serão aceitos')
-    else:
-        print(f'Número digitado: {n}')
-        break
--- secao4/05-.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        n = int(input('Digite um valor inteiro: '))
-    except ValueError:
-        print('Apenas valores inteiros são aceitos...')
-    else:
-        print(f'A quinta parte de {n} é: {n/5}')
-        break
