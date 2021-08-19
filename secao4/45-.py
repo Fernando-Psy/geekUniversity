78863ee4fc3ead071e7439f6ad5565170f98b0e1
JamesFelipe <77255316+SemajOliveira@users.noreply.github.com>
2021-08-18 22:52:14 -0300

    newCode

---
+++ secao4/45-.py
@@ -0,0 +1,2 @@
+letra = input('Digite uma letra maiúscula: ').strip().upper()
+print(f'Letra em minúsculo: {letra.lower()}')
---
+++ secao4/48-.py
@@ -0,0 +1,6 @@
+segundos = int(input('Digite os segundos inteiros: '))
+print(f'{segundos} segundos equivale a {segundos // 3600} horas')
+print(f'{segundos} segundos equivale a {segundos // 60}')
+print(f'{segundos} segunos')
+print(f'Formando {segundos//3600}:{segundos//60}:{segundos} ')
+
